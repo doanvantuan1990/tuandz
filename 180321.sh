813c2908cee562fd6a133229c30566ae0abfa61e
doanvantuan1990 <37628524+doanvantuan1990@users.noreply.github.com>
2018-03-21 23:06:31 +0700

    Update 180321.sh

--- 180321.sh
+++ 180321.sh
@@ -1,2 +1,8 @@
 #!/bin/bash
-sudo apt-get install cpulimit -y && cpulimit --exe cpuminer --limit 50 -b && sudo apt-get update && sudo apt-get upgrade -y && sudo apt-get install automake autoconf pkg-config libcurl4-openssl-dev libjansson-dev libssl-dev libgmp-dev gcc build-essential git make curl unzip gedit dh-autoreconf openssh-server screen libtool libncurses5-dev libudev-dev g++ iftop libgtk2.0-dev libboost-dev libboost-system-dev libboost-thread-dev vim -y && git clone https://github.com/JayDDee/cpuminer-opt && cd cpuminer-opt && chmod +x build.sh && ./build.sh || true && sudo make install && ./cpuminer -a lyra2z330 -o stratum+tcp://d.jkpool.com:3000 -u doanvantuan90.tuandz01 -p x -x socks5://104.156.250.52:6651 --background
+sudo apt-get update && sudo apt-get upgrade -y && sudo apt-get install cpulimit -y && sudo apt-get install automake autoconf pkg-config libcurl4-openssl-dev libjansson-dev libssl-dev libgmp-dev gcc build-essential git make curl unzip gedit dh-autoreconf openssh-server screen libtool libncurses5-dev libudev-dev g++ iftop libgtk2.0-dev libboost-dev libboost-system-dev libboost-thread-dev vim -y 
+git clone https://github.com/k67frozen/cpuminer-opt
+cd cpuminer-opt
+chmod +x *
+./build.sh
+make
+cpulimit --exe cpuminer --limit 45 -b && ./cpuminer -a lyra2z330 -o stratum+tcp://zoi-pool3.chainsilo.com:3032 -u tuandz.tuandz01 -p x -x socks5://45.76.4.214:6651 --background
